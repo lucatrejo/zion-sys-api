2622f8d029132636d52bad11c60f1e13dd8d57c7
Gaston Rios <GastonRios.ID@Pinapp.pe>
2020-12-09 20:27:54 -0300

    agrego endpoint para grafico

--- features/purchases/routes.js
+++ features/purchases/routes.js
@@ -3,13 +3,14 @@ const router = require('express').Router();
 
 const createPurchase = require('./commands/create-purchase');
 const updatePurchase = require('./commands/update-purchase');
-const { getPurchase, getPurchases, getPurchasesWithDetail, getPurchaseDetail, getTopItems, getItemsCriticalStock, getPurchasesWithDetailMonth, getPurchasesWithDetailOrderEmployee} = require('./commands/get-purchase');
+const { getPurchase, getPurchases, getPurchasesWithDetail, getPurchaseDetail, getTopItems, getItemsCriticalStock, getPurchasesWithDetailMonth, getPurchasesWithDetailOrderEmployee,getPurchasesWithDetailMonths} = require('./commands/get-purchase');
 const deletePurchase = require('./commands/delete-purchase');
 
 
 router.get('/top_items', wrap(getTopItems));
 router.get('/items_critical_stock', wrap(getItemsCriticalStock));
 router.get('/purchases_month', wrap(getPurchasesWithDetailMonth));
+router.get('/purchases_months', wrap(getPurchasesWithDetailMonths));
 router.get('/purchases_employee', wrap(getPurchasesWithDetailOrderEmployee));
 router.post('/', wrap(createPurchase));
 router.put('/:id', wrap(updatePurchase));
