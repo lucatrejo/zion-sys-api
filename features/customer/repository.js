06f603f47302f5cedebd4543d089e2e75bbda879
Luca Trejo <luca.trejo@mercadolibre.com>
2021-02-13 23:41:50 -0300

    customize account detail response

--- features/customer/repository.js
+++ features/customer/repository.js
@@ -93,7 +93,11 @@ async function getAccount(customerId) {
 
 async function getDetailAccount(accountId) {
   const customer = await knex(TABLE_NAME_DETAIL_ACCOUNT)
-    .select()
+    .select(
+      knex.raw(
+        "sale_id, to_char(created_at,'DD/MM/YYYY') as date, status"
+      )
+    )
     .where('account_id', accountId);
   return customer;
 }
