c00b9943ebb0407e2f28fda9aae3c8b8181e8491
Luca Trejo <luca.trejo@mercadolibre.com>
2021-03-10 12:37:38 -0300

    fix ssl connection pg

--- db/knexfile.js
+++ db/knexfile.js
@@ -57,7 +57,7 @@ module.exports = {
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
-      ssl: true
+      ssl: { rejectUnauthorized: false }
     },
     pool: {
       min: 2,
